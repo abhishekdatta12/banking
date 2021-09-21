9cb2b203dd14c2d2e6a9b097393162ec07014b6f
abhishekdatta12 <81951956+abhishekdatta12@users.noreply.github.com>
2021-09-21 18:18:20 +0530

    Virtual MySQL DB4Free Connect

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,9 +1,9 @@
 <?php
 
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "bank";
+$dbServername = "db4free.net:3306";
+$dbUsername = "abhishekdatta1";
+$dbPassword = "lifeline";
+$dbName = "bank_spark";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 ?>
